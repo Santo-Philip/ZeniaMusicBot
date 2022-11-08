e07dd022e28373e56ed312431840141e20fefe88
Santo-Philip <Santop7510@gmail.com>
2022-11-08 22:42:50 +0530

    only errors edition

---
+++ ZeniaMusic/__init__.py
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from ZeniaMusic.core.bot import ZeniaBot
+from ZeniaMusic.core.dir import dirr
+from ZeniaMusic.core.git import git
+from ZeniaMusic.core.userbot import Userbot
+from ZeniaMusic.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = ZeniaBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
